0dfb18c67c5cdc685b63c2a0811d367f3dd312ad
DongYoonKim <dongyoon.roy.kim@gmail.com>
2023-01-10 10:42:50 -0800

    LayOut of AddApplication View Control is added.

---
+++ JobKer/AddApplicationVC.swift
@@ -0,0 +1,18 @@
+//
+//  AddApplicationVC.swift
+//  JobKer
+//
+//  Created by Roy's Saxy MacBook on 1/10/23.
+//
+
+
+import UIKit
+import Foundation
+
+class AddApplicationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
