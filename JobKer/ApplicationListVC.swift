342eb80162207a92603a3e8320c605970a6a9eeb
DongYoonKim <dongyoon.roy.kim@gmail.com>
2023-01-10 10:10:20 -0800

    Having trouble with using Charts, so I temporarily removed the package.

---
+++ JobKer/ApplicationListVC.swift
@@ -0,0 +1,17 @@
+//
+//  ApplicationListVC.swift
+//  JobKer
+//
+//  Created by Roy's Saxy MacBook on 1/10/23.
+//
+
+import Foundation
+import UIKit
+
+class ApplicationListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
--- JobKer/MainVC.swift
+++ JobKer/MainVC.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Foundation
+//import Charts
 
 class MainVC: UIViewController {
 
