e0b73e35e2d81d5bc8a6c5ea19f42540dfc5ca94
DongYoonKim <dongyoon.roy.kim@gmail.com>
2023-01-13 10:50:34 -0800

    plus button moved onto the navigation bar of ApplicationList.

--- JobKer/ApplicationListVC.swift
+++ JobKer/ApplicationListVC.swift
@@ -9,10 +9,21 @@ import Foundation
 import UIKit
 
 class ApplicationListVC: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: .plus, style: .done, target: self, action:)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .done, target: self, action: #selector(openAddApplication))
+    }
+    
+    @objc fileprivate func openAddApplication(sender: UIBarButtonItem){
+        let story = UIStoryboard(name: "ApplicationList", bundle: nil)
+        let controller = story.instantiateViewController(identifier: "AddApplicationVC") as! AddApplicationVC
+//        self.present(controller, animated: true, completion: nil)
+        let navigation = UINavigationController(rootViewController: controller)
+        self.view.addSubview(navigation.view)
+        self.addChild(navigation)
+        navigation.didMove(toParent: self)
     }
 }
 
